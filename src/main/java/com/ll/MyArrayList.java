c8b5d498fe47b550a602c8768c841b68d5d91213
ujineeee <yujin11006@naver.com>
2023-04-19 15:15:57 +0900

    feat : remove, contains, indexOf, clear

--- src/main/java/com/ll/MyArrayList.java
+++ src/main/java/com/ll/MyArrayList.java
@@ -27,6 +27,55 @@ public class MyArrayList<E> {
     public E get(int index) {
         if(index >= data.length || index < 0)
             throw new IndexOutOfBoundsException();
+
         return (E) data[index];
+    }
+
+    public E remove(int index) {
+        if(index >= data.length || index < 0)
+            throw new IndexOutOfBoundsException();
+
+        E previous = (E) data[index];
+
+        Object[] newData = new Object[data.length - 1];
+        for(int i = 0; i < index; i++) {
+            newData[i] = data[i];
+        }
+        for(int i = index+1; i < data.length; i++) {
+            newData[i - 1] = data[i];
+        }
+
+        data = newData;
+
+        return previous;
+    }
+
+    public boolean contains(Object o) {
+        for(Object value : data) {
+            if(value.equals(o))
+                return true;
+        }
+
+        return false;
+    }
+
+    public int indexOf(Object o) {
+        for(int i = 0; i < data.length; i++) {
+            if(data[i].equals(o))
+                return i;
+        }
+
+        return -1;
+    }
+
+    public boolean isEmpty(){
+        if(data == null || data.length == 0)
+            return true;
+
+        return false;
+    }
+
+    public void clear (){
+        data = new Object[0];
     }
 }
--- src/test/java/MyArrayListTest.java
+++ src/test/java/MyArrayListTest.java
@@ -34,4 +34,41 @@ public class MyArrayListTest {
         assertEquals("Element1", list.get(0));
         assertEquals("Element2", list.get(1));
     }
+
+    @Test
+    void testRemove() {
+        list.add("Element1");
+        list.add("Element2");
+        list.add("Element3");
+        assertEquals("Element2", list.remove(1));
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    void testContains() {
+        list.add("Element1");
+        list.add("Element2");
+        assertTrue(list.contains("Element1"));
+        assertFalse(list.contains("Element3"));
+    }
+
+    @Test
+    void testIndexOf() {
+        list.add("Element1");
+        list.add("Element2");
+        list.add("Element1");
+        assertEquals(0, list.indexOf("Element1"));
+        assertEquals(1, list.indexOf("Element2"));
+        assertEquals(-1, list.indexOf("Element3"));
+    }
+
+    @Test
+    void testClear() {
+        list.add("Element1");
+        list.add("Element2");
+        list.add("Element3");
+        list.clear();
+        assertEquals(0, list.size());
+        assertTrue(list.isEmpty());
+    }
 }
